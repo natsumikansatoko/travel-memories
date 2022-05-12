259c8b206f12385ede34dad269959b9afa03f517
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-05-12 12:44:52 +0000

    [Add]日本地図

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+// 以下追記
 const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    jquery: 'jquery/src/jquery',
-  })
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery',
+        jquery: 'jquery/src/jquery',
+        "window.jQuery": "jquery"
+    })
 )
+// ここまで
 
 module.exports = environment
