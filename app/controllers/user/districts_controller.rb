eb214443688dd55df0740cfb58506b6491f3330c
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-05-21 12:21:58 +0000

    [Add]都道府県別投稿一覧

--- app/controllers/user/districts_controller.rb
+++ app/controllers/user/districts_controller.rb
@@ -1,7 +1,15 @@
 class User::DistrictsController < ApplicationController
   def index
+
   end
 
   def show
+    @district = District.find(params[:id])
+    @memories = @district.memories.where(params[:id])
+  end
+
+  private
+  def district_params
+    params.require(:district).permit(:memory_id, :name)
   end
 end
