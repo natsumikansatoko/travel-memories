72e6b04fbc4ab3227dc706f74de36682861c187e
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-05-22 05:26:19 +0000

    [Add]マイページ

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -2,20 +2,30 @@ class User::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @memories = @user.memories
-    
     favorites = Favorite.where(user_id: current_user.id).pluck(:memory_id)
-    @favorite_list = Memory.find(favorites)
+    @favorites = Memory.find(favorites)
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
   end
 
   def withdrawal
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :is_active)
   end
 end
