ea580c0a98ccc524f13218e9939f0c485f49488a
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-06-04 13:03:15 +0000

    [ADD]修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1e15b59f2dca0a57d7521efba20b61af7effdb6d83316810f6a38d68c62ccc9947c48406c3885890f6fe57f9986115b25d5a98b9a600de7870761eb671257599'
